a01ea0c6c8063acf4a9f3e0f5927e44630e778d3
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-11-21 06:51:12 -0500

    adding templates and new routes

--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,19 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+class Car:
+    def __init__(self, model, make, description, milage):
+        self.model = model
+        self.make = make
+        self.description = description
+        self.milage = milage
+
+cars = [
+    Car('Camaro', 'Chevy', 'Lorem ipsum do.', 20000),
+    Car('Mustang', 'Ford', 'Lorem ipsum do.', 35000),
+    Car('400z', 'Nissan', 'Lorem ipsum do.', 12000),
+]
+
+def cars_index(request):
+    return render(request, 'cars/index.html', {'cars': cars})
