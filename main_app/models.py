ff9172e98d5adbe745a830f44231eb32a6530174
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-11-21 18:30:41 -0500

    created model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    model = models.CharField(max_length=100)
+    make = models.CharField(max_length=100)
+    description = models.TextField()
+    milage = models.IntegerField()
+    price = models.IntegerField()
+
